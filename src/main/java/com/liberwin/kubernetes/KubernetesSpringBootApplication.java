ffe41070a723ace72040fc905ebe8bb553ec98d5
Saravanakumar <saravg2012@gmail.com>
2020-07-12 20:31:15 -0500

    k8s-first commit

---
+++ src/main/java/com/liberwin/kubernetes/KubernetesSpringBootApplication.java
@@ -0,0 +1,27 @@
+package com.liberwin.kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KubernetesSpringBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KubernetesSpringBootApplication.class, args);
+		System.out.println("HEllo K8s");
+	}
+
+
+	@GetMapping("/message")
+	public String getMessage() {
+
+		return "Welocme to Tech Primes !!";
+	}
+
+
+
+}
---
+++ src/test/java/com/liberwin/kubernetes/KubernetesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liberwin.kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
