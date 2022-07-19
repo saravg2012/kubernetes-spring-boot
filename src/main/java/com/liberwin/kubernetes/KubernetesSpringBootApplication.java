8a16b1d4b1ab030547826344605c0ea0c5720988
saravg2012@gmail.com <saravg2012@gmail.com>
2022-07-19 15:25:30 -0400

    b2 change

--- src/main/java/com/liberwin/kubernetes/KubernetesSpringBootApplication.java
+++ src/main/java/com/liberwin/kubernetes/KubernetesSpringBootApplication.java
@@ -12,7 +12,7 @@ public class KubernetesSpringBootApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(KubernetesSpringBootApplication.class, args);
-		System.out.println("Hello branch4445");
+		System.out.println("Hello branch444566666");
 		System.out.println("Hello branch3");
 	}
 
